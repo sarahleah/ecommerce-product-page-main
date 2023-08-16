bb1a84bb5389bb29bef6b96209be705fd1b35dc4
sarahleah <sarah.leah.andreas@gmail.com>
2023-08-16 19:41:44 +0200

    sort out nav a little

--- src/Nav.js
+++ src/Nav.js
@@ -8,14 +8,16 @@ import avatar from '../images/image-avatar.png';
 const Nav = () => {
   return (
     <nav>
-      <img src={logoSvg} alt="logo"></img>
-      <ul>
-        <li>Collections</li>
-        <li>Men</li>
-        <li>Women</li>
-        <li>About</li>
-        <li>Contact</li>
-      </ul>
+      <div className="left">
+        <img src={logoSvg} alt="logo"></img>
+        <ul>
+          <li>Collections</li>
+          <li>Men</li>
+          <li>Women</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </div>
       <div className="right">
         <img src={cartIcon} alt="cart"></img>
         <img className="avatar" src={avatar} alt="it's you!"></img>
